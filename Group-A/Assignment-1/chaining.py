ffbb24599c599311ec0ced644e232e372a067288
Garo-U <adityadurugkar07@gmail.com>
2023-04-19 08:23:21 +0530

    changes

--- Group-A/Assignment-1/chaining.py
+++ Group-A/Assignment-1/chaining.py
@@ -6,18 +6,26 @@ class HashTable:
         self.values = [None] * size
     
     def hash_function(self, key):
-        return hash(key) % self.size
-    
+        count = 0
+        for _ in key:
+            count += 1
+        hash_value = count % self.size
+        
+        return hash_value
+
     def hash_insert(self, key, value):
         index = self.hash_function(key)
         if self.keys[index] is None:
             self.keys[index] = [key]
             self.values[index] = [value]
+
         elif self.keys[index] == key:
             self.values[index] = value
+
         elif isinstance(self.keys[index], list):
             self.keys[index].append(key)
             self.values[index].append(value)
+        
         else:
             # if self.keys[index] is not None and not a list
             # create a new list and rehash the previous item
@@ -37,6 +45,28 @@ class HashTable:
             else:
                 print(f"Index {i}: {self.keys[i]}: {self.values[i]}")
 
+    def hash_search(self, key):
+        index = self.hash_function(key)
+        comparisons = 1
+
+        while self.keys[index] is not None:
+            if isinstance(self.keys[index], list):
+                # Search within a list of keys at the same index
+                for i in range(len(self.keys[index])):
+                    if self.keys[index][i] == key:
+                        print(f"Found {key} with value {self.values[index][i]}")
+                        return comparisons
+                    comparisons += 1
+            elif self.keys[index] == key:
+                print(f"Found {key} with value {self.values[index]}")
+                return comparisons
+            comparisons += 1
+            index = (index + 1) % self.size
+
+        print(f"{key} not found")
+        return comparisons
+
+
 
 # n = int(input("size of the table : "))
 lalo = HashTable(10)
@@ -44,12 +74,12 @@ lalo = HashTable(10)
 for i in range(10):
     
     #& Yeah Biatch !!!
-    name = ''.join(random.choices(string.ascii_uppercase, k=5)) 
-    contact_no = random.randint(1000000000, 9999999999)
+    # name = ''.join(random.choices(string.ascii_uppercase, k=5)) 
+    # contact_no = random.randint(1000000000, 9999999999)
 
     #~ To do manually shit contact numbers
-    # name = input("Enter contact name : ")
-    # contact_no = int(input(f"Enter Contact number of {name}"))
+    name = input("Enter contact name : ")
+    contact_no = int(input(f"Enter Contact number of {name}"))
 
     lalo.hash_insert(name,contact_no)
 
--- Group-A/Assignment-1/linear_probing.py
+++ Group-A/Assignment-1/linear_probing.py
@@ -9,13 +9,13 @@ class Linear_probing:
         self.values = [None] * self.size
 
     def hash_function(self, key):
-        
-        '''
-        we will be hashing keys as keys are unique 
-        and values may repeat to minimize collisions
-        '''
 
-        return hash(key) % self.size
+        count = 0
+        for _ in key:
+            count += 1
+        hash_value = count % self.size
+        
+        return hash_value
 
     def hash_insert(self, key , value):
         
@@ -34,7 +34,16 @@ class Linear_probing:
             elif None not in self.keys:
                 print("Hashtable Overflow")
 
-
+            elif key in self.keys:
+                print('already present in hashtable')
+                ask = input("Wanna update the keys with existing \
+                            the previous will be overwritten (y/n)").capitalize()
+                if ask == 'Y':
+                    print(f"Previous value for key '{key}': {self.values[index]}")
+                    self.values[index] = value
+                else:
+                    pass
+        
             else:
                 pass
         
@@ -45,22 +54,36 @@ class Linear_probing:
         for i in range(self.size):
             print("{} : [{} - {}]".format(i,self.keys[i],self.values[i]))
 
+    def hash_search(self, key):
+        index = self.hash_function(key)
+        comparisons = 1
+        
+        while self.keys[index] is not None:
+            if self.keys[index] == key:
+                print(f"Found {key} with value {self.values[index]}")
+                return comparisons
+            comparisons += 1
+            index = (index + 1) % self.size
+            
+        print(f"{key} not found")
+        return comparisons
+
 
 
 if __name__ == '__main__':
 
     # n = int(input("size of the table : "))
-    lalo = Linear_probing(10)
+    lalo = Linear_probing(5)
 
-    for i in range(10):
+    for i in range(5):
         
         #& Yeah Biatch !!!
-        name = ''.join(random.choices(string.ascii_uppercase, k=5)) 
-        contact_no = random.randint(1000000000, 9999999999)
+        # name = ''.join(random.choices(string.ascii_uppercase, k = 5)) 
+        # contact_no = random.randint(1000000000, 9999999999)
 
         #~ To do manually shit contact numbers
-        # name = input("Enter contact name : ")
-        # contact_no = int(input(f"Enter Contact number of {name}"))
+        name = input("Enter contact name : ")
+        contact_no = int(input(f"Enter Contact number of {name}"))
 
         lalo.hash_insert(name,contact_no)
 
