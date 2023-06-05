b882e1fed083df7167a3ba05e1785551aea6754c
Aditya Durugkar <adityadurugkar07@gmail.com>
2023-06-05 23:01:33 +0530

    new

--- Group-A/Assignment-1/linear_probing.py
+++ Group-A/Assignment-1/linear_probing.py
@@ -58,6 +58,30 @@ class Linear_Probing:
 
         print(f"{key} not found")
         return
+    
+if __name__ == "__main__":
+
+    n = int(input("size of hashtable : "))
+    rick = Linear_Probing(n)
+    
+
+    for i in range(n):
+
+        rick.hash_insert(random_name, random_number)
+
+    name = input("Contact_name : ")
+    number = int(input("Contact_number : "))
+
+    rick.hash_insert(name, number)
+
+    print(f"Hashtable of Linear Probing :- {rick.print_table()} \n\n")
+
+
+    _name = input("Any contact you wanna search for : ")
+
+
+    print(rick.hash_search(_name))
+
 
 
 
